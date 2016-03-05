cf6f8749b5bf6533e9d897906f74eeee62928262
schottkey <schottkey@gmail.com>
2016-03-05 09:29:02 +0000

    initial commits - learning go

---
+++ first-steps/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello" + " world")
+	fmt.Println("1.0 / 13.0 =", 1.0/13.0)
+	fmt.Println(true && false || true)
+	fmt.Println(!!true)
+}
