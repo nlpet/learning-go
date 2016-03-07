96ce2b9ad35ebf3d2fd7d388cbe20454445eaf4d
norapetrova <nora.petrova@cde.catapult.org.uk>
2016-03-07 12:32:22 +0000

    more exercises - up to timers

---
+++ first-steps/non-blocking-channel-operations.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	messages := make(chan string, 1)
+	signals := make(chan bool)
+
+	//messages <- "message"
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("send message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+
+}
