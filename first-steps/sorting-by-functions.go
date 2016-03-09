b240aca5f2a6e36504e8e2e8ee1a461225450e4f
Nora Petrova <schottkey@gmail.com>
2016-03-09 14:07:22 +0000

    Update sorting-by-functions.go

--- first-steps/sorting-by-functions.go
+++ first-steps/sorting-by-functions.go
@@ -6,42 +6,32 @@ import (
 	"strings"
 )
 
+const vowels string = "aeiou"
+
 type ByLength []string
 type ByNumVowels []string
 
 // By length
-func (s ByLength) Len() int {
-	return len(s)
-}
-
-func (s ByLength) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s ByLength) Less(i, j int) bool {
-	return len(s[i]) < len(s[j])
-}
+func (s ByLength) Len() int           { return len(s) }
+func (s ByLength) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s ByLength) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
 
 // By number of vowels
-func (s ByNumVowels) Len() int {
-	vowels := "aeiou"
+func (s ByNumVowels) Len() int      { return len(s) }
+func (s ByNumVowels) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s ByNumVowels) Less(i, j int) bool {
+	return numVowels(s[i]) > numVowels(s[j])
+}
+
+func numVowels(s string) int {
 	numVowels := 0
 
 	for _, r := range s {
-		c := string(r)
-		if strings.Contains(vowels, c) {
+		if strings.Contains(vowels, string(r)) {
 			numVowels++
 		}
 	}
 	return numVowels
-}
-
-func (s ByNumVowels) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s ByNumVowels) Less(i, j int) bool {
-	return len(s[i]) < len(s[j])
 }
 
 func main() {
